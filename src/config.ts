e45ecd84439d7872f34a4ae87a60414904ac99e7
satyaK0729 <satyahi729@gmail.com>
2025-12-30 08:06:05 +0530

    updated Projects

--- src/config.ts
+++ src/config.ts
@@ -18,22 +18,29 @@ export const siteConfig = {
     github: "https://github.com/satyaK0729",
   },
   aboutMe:
-    "Hi, I'm K. Satya Narayana Rao — Iam a Computer Science major specializing in Ai and Ml at SRM University, AP. I build few full-stack web applications using technologies like React JS, Node.js, Express.js, Javascript . My work includes projects like Photography contests with voting, blog platform, and biomedical image classification. I'm eager to grow as a software developer and contribute some impactful, real-world solutions.",
-  skills: ["Skills :-","Javascript", "React", "Node.js","MERN", "Python", "DBMS", "GIT"],
+    `Hi, I'm K. Satya Narayana Rao — I am a Computer Science major specializing in AI and ML at SRM University, AP. I build full-stack web applications using technologies like React JS, Node.js, Express.js, and Javascript. My work includes projects like Photography contests with voting, blog platform, and biomedical image classification. I'm eager to grow as a software developer and contribute some impactful, real-world solutions.`,
+  skills: ["Python", "Javascript", "React", "MERN", "BigQuery", "(GCP) Google Cloud Platform ", "DBMS", "GitHub "],
   projects: [
     {
-      name: "Photography Contest",
+      name: "Photography Contest with voting",
       description:
         "Developed a full-stack Photography Contest Platform using MERN. With Authentication which enabling users to participate and vote in live contests. Integrated image uploads, real-time voting, and admin controls for winner selection. Implemented email notifications and a responsive, user-friendly interface.",
       link: "https://photography-contest-phi.vercel.app",
-      Tools: ["React", "Node.js", "Express.js", "MongoDB", "Redux"],
+      skills: ["React", "Node.js", "Express.js", "MongoDB", "Redux"],
     },
     {
       name: "BigQuery: Target E-Commerce Performance Analytics",
       description:
         "Developed and implemented an end-to-end analytics solution on a dataset of over 100,000 e-commerce orders for a target e-commerce platform using Google BigQuery. The project involved ingesting, normalizing, and analyzing data from 8 interconnected tables, ultimately generating key performance indicators (KPIs) for optimizing delivery performance and logistics",
       link: "https://github.com/satyaK0729/Target-Sql-Analysis",
-      skills: ["BigQuery", "Google Cloud PLatform (GCP)", "Github"],
+      skills: ["BigQuery", "Google Cloud PLatform (GCP)", "SQL"],
+    },
+    {
+      name: "Automated ETL Data Pipeline on Sales Data in GCP",
+      description:
+        "Developed and implemented an end-to-end analytics solution on a dataset of over 100,000 e-commerce orders for a target e-commerce platform using Google BigQuery. The project involved ingesting, normalizing, and analyzing data from 8 interconnected tables, ultimately generating key performance indicators (KPIs) for optimizing delivery performance and logistics",
+      link: "https://github.com/satyaK0729/Target-Sql-Analysis",
+      skills: ["Google Cloud Storage","Google Cloud Functions","BigQuery", "LookerStudio","Airflow","Python Flask"],
     },
     {
       name: "Robust Biomedical Image Classification Using Fuzzy Weighted Ensemble of Deep Neural Networks.",
@@ -64,14 +71,14 @@ export const siteConfig = {
   education: [
     {
       school: "SRM UNIVERSITY, AP",
-      degree: "Bachelor of Techonolgy  B.Tech" ,
+      degree: "Bachelor of Techonolgy  B.Tech",
       dateRange: "2021 - 2025",
       achievements: [
         "Course: Computer Science and Engineering",
         "Specilization: Ai and Ml",
       ],
     }
-    
+
   ],
   resumeLink: "https://drive.google.com/file/d/1FeFD5ooZnzT2yWRE9_BFF5aG8aJdB9Ht/view?usp=sharing",
 };
